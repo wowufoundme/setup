ea364c67ec578d469215543da58930b33239f3dd
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-22 20:03:37 +0530

    initial commit

---
+++ src/components/LeftBase.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './LeftBase.styles.css';
+
+const LeftBase = () => {
+  return (
+    <div className='__left-container'></div>
+  )
+}
+
+export default LeftBase;
---
+++ src/components/RightBase.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './RightBase.styles.css';
+
+const RightBase = () => {
+  return (
+    <div className='__right-container'>
+      <h1>Right component</h1>
+    </div>
+  )
+}
+
+export default RightBase;
