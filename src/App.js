3725b480b31d6a5c7bac82326deb2cd5d1ef6e3a
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-25 04:56:07 +0530

    other major changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import LeftBase from './components/LeftBase.component';
-import RightBase from './components/RightBase.component';
+import ProfileImage from './components/ProfileImage';
+import Lead from './components/Lead';
+import Description from './components/Description';
+import ThemeSwitcher from './components/ThemeSwitcher';
 
-function App() {
+const App = () => {
+
+  const [theme, setTheme] = useState('');
+
+  const changeTheme = e => {
+    if (theme === '') {
+        setTheme('dark');
+    } else {
+        setTheme('');
+    }
+    document.documentElement.setAttribute("data-theme", theme);
+  }
+
+  useEffect(() => {
+      document.documentElement.setAttribute("data-theme", theme);
+  })
+
   return (
     <div className="App">
-      <LeftBase />
-      <RightBase />
+      <div className="container">
+        <ProfileImage />
+        <Lead />
+        <Description />
+        <ThemeSwitcher theme={theme} changeTheme={changeTheme}/>
+      </div>
     </div>
   );
 }
