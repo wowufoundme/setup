da59d2e0c1aa79115519e0b1e6a705cf19ec8e8e
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-25 14:12:15 +0530

    header edits

--- src/components/Lead.js
+++ src/components/Lead.js
@@ -6,7 +6,14 @@ const Lead = () => {
   return (
     <div className="lead-container">
       <h3 className="title">Shubhkirti Sharma</h3>
-      <p className="description">Full Stack Developer | KDrama Fanatic | Produces music in free time</p>
+      <p className="description">
+        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15" className="mh-10"><path d="M10.146 10.146l-.353.354.707.707.354-.353-.708-.708zM13.5 7.5l.354.354.353-.354-.353-.354-.354.354zm-2.646-3.354l-.354-.353-.707.707.353.354.708-.708zm-6.708 6.708l.354.353.707-.707-.353-.354-.708.708zM1.5 7.5l-.354-.354-.353.354.353.354L1.5 7.5zm3.354-2.646l.353-.354-.707-.707-.354.353.708.708zm6 6l3-3-.708-.708-3 3 .708.708zm3-3.708l-3-3-.708.708 3 3 .708-.708zm-9 3l-3-3-.708.708 3 3 .708-.708zm-3-2.292l3-3-.708-.708-3 3 .708.708zm6.153-6.436l-2 12 .986.164 2-12-.986-.164z" fill="#74b9ff"></path></svg> Frontend React Developer 
+        <span className="separator">|</span> 
+        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15" className="mh-10"><path d="M2.5 11.5v2m10-2v2M1 13.5h3m7 0h3M.5 2.5v8a1 1 0 001 1h12a1 1 0 001-1v-8a1 1 0 00-1-1h-12a1 1 0 00-1 1z" stroke="#74b9ff"></path></svg>
+        KDrama Fanatic 
+        <span className="separator">|</span> 
+        <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" width="15" height="15" className="mh-10"><path d="M1.5 9.5h1a1 1 0 011 1v3a1 1 0 01-1 1h-1a1 1 0 01-1-1v-3a1 1 0 011-1zm0 0v-3a6 6 0 1112 0v3m0 0h-1a1 1 0 00-1 1v3a1 1 0 001 1h1a1 1 0 001-1v-3a1 1 0 00-1-1z" stroke="#74b9ff"></path></svg>
+        Produces music in free time</p>
     </div>
   )
 }
